779bab23f5e9f7a428fc49ba831c31b81f316f04
yankaduchkevich <kadushkevichyanolegovich@gmail.com>
2022-10-28 01:47:23 +0300

    First version of this task. Added condition for params and sort function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,18 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  if (param === 'asc') {
+    sortASC(arr);
+  } else if (param === 'desc') {
+    sortASC(arr).reverse();
+  }
 }
+
+function sortASC(array) {
+  // новый массив (копия arr)
+  let newArr = array.slice();
+  // сортитруем новый массив в правильном порядке
+  newArr.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+  return newArr;
+}
+
